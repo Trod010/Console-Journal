923faaa0329655ff3bf5c13243891b86f9d05ce1
Trod010 <Tmrod1318@gmail.com>
2019-04-08 15:14:04 -0400

    Edit 1

---
+++ Journal/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Journal
+{
+    class Database
+    {
+        private List<Entry> entries;
+
+        public Database()
+        {
+            entries = new List<Entry>();
+        }
+
+        public void AddEntry(DateTime occurs, string text)
+        {
+            entries.Add(new Entry(occurs, text));
+        }
+
+        public List<Entry> FindEntries(DateTime date, bool byTime)
+        {
+            List<Entry> found = new List<Entry>();
+            foreach(Entry entry in entries)
+            {
+                if (((byTime) && (entry.Occurs == date)) // filters by date and time
+                    ||
+                    ((!byTime) && (entry.Occurs.Date == date.Date))) found.Add(entry); // this filters by date
+            }
+            return found;
+        }
+    }
+}
---
+++ Journal/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Journal
+{
+    class Entry
+    {
+        public DateTime Occurs { get; set; }
+        public string Text { get; set; }
+
+        public Entry(DateTime occurs, string text)
+        {
+            Occurs = occurs;
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return Occurs + " " + Text;
+        }
+    }
+}
